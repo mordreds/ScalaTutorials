5f41f499c19626610a2ae96840b394580ced3197
osborne Yaw <osborne.mordred@gmail.com>
2022-05-11 16:44:44 +0000

    Advert Code Puzzle Completed

--- src/main/scala/puzzle/Puzzle.scala
+++ src/main/scala/puzzle/Puzzle.scala
@@ -3,56 +3,104 @@ package puzzle
 import scala.collection.mutable.ListBuffer
 
 object Puzzle {
-
-  def reverseArray(arr:List[Int]) = {
-
-    var list = new ListBuffer[Int]()
-
-    val arrSize= arr.length
-
-    for (item <- arr) list += item
-
-    list
+  
+  enum ORDER {
+    case MOST_COMMON
+    case LEAST_COMMON
   }
 
   def main(args: Array[String]): Unit = {
 
-    val binaryList = List("00100", "11110", "10110", "10111", "10101", "01111",
-      "00111", "11100", "10000", "11001", "00010", "01010")
+    val binaryList = List(
+      "00100",
+      "11110",
+      "10110",
+      "10111",
+      "10101",
+      "01111",
+      "00111",
+      "11100",
+      "10000",
+      "11001",
+      "00010",
+      "01010"
+    )
 
-    val aList = List(1,2,3,5,4,4,5)
+    val gammaRate = gammaRateBinaryList(binaryList)
 
-    println(reverseArray(aList))
+    val epsilonRate = epsilonRateBinaryList(binaryList)
+
+    val powerConsumption = gammaRate * epsilonRate
+
+    println(s"Power Consumption is :: $powerConsumption Watts")
   }
 
-  def calculateGammaRate(aList:List[String]) = {
+  def gammaRateBinaryList(aList:List[String]) : Long = {
+    val res = for (idx <- 0 until aList.head.length)
+      yield retrieveMostFrequentBitInColumn(aList,idx,ORDER.MOST_COMMON)
 
-    val mostFrequentBitList = new ListBuffer[Int]()
+    val gammaRate: String =  res.foldLeft("")(_+_)
 
-    for (index <- 0 until 4 )
-      retrieveMostFrequentBitInColumn(aList, index)
+    val decimalNumber: DecimalNumber = new DecimalNumber()
 
-    mostFrequentBitList
+    decimalNumber.binaryToDecimal(gammaRate)
   }
 
+  def epsilonRateBinaryList(aList: List[String]) : Long =
+  {
+    val res = for (idx <- 0 until aList.head.length)
+      yield retrieveMostFrequentBitInColumn(aList,idx,ORDER.LEAST_COMMON)
 
-  def epsilonRate(aList: List[String]):  Int = ???
+    val epsilonRate = res.foldLeft("")(_+_)
 
-  def powerConsumption: Int = ???
+    val decimalNumber: DecimalNumber = new DecimalNumber()
 
-  def retrieveMostFrequentBitInColumn(aList: List[String], listColumn: Int): Int = {
+    decimalNumber.binaryToDecimal(epsilonRate)
+  }
 
-    val columnAccumulator = new ListBuffer[Char]()
+  def retrieveMostFrequentBitInColumn(aList: List[String], listColumn: Int, order: ORDER) : Char = {
 
-    for (a <- aList)
-      columnAccumulator += a.charAt(listColumn)
+    // group list elements into map of unique key and list of its occurrences
+    // output :: HashMap(1 -> List(1, 1, 1, 1, 1, 1, 1), 0 -> List(0, 0, 0, 0, 0))
+    val columnAccumulator: Map[Char, List[Char]] = aList.map(bit => bit.charAt(listColumn)).groupBy(identity)
 
-    val mostCommonItem: Int = columnAccumulator
-      .groupBy(identity)
-      .view.mapValues(_.size)
-      .maxBy(_._2)
-      ._1
+    order match {
+       case ORDER.MOST_COMMON => columnAccumulator.view
+         .mapValues(_.size) // return Map( 1 -> 7, 0 -> 5)
+         .maxBy(_._2) // get highest map value
+         ._1 // return its key
 
-    mostCommonItem
+       case ORDER.LEAST_COMMON => columnAccumulator.view
+         .mapValues(_.size)
+         .minBy(_._2)
+         ._1
+     }
+  }
+}
+
+// didnt find a direct conversion so copied this class from online
+class DecimalNumber()
+{
+  def binaryToDecimal(number: String): Long = {
+    // Assuming that number contains 0,1s
+    // Used to store result
+    var result: Long = 0
+    var bit: Int = 0
+    var n: Int = number.length() - 1
+    // Display Binary number
+    // Execute given number in reverse order
+    while (n >= 0)
+    {
+      if (number.charAt(n) == '1')
+      {
+        // When get binary 1
+        result += 1.<<(bit)
+      }
+      n = n - 1
+      // Count number of bits
+      bit += 1
+    }
+    // Display decimal result
+    result
   }
 }
